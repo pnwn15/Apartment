3364cd264572376374391413828bdcf0c356bfc6
Kim Boolkinee <boolkinee9@gmail.com>
2025-03-28 08:37:17 +0700

    เพิ่มหน้า billlist กับ  MeterReading

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import DashboardPage from './Pages/Dashboard/DashboardPage';
+import MeterReading from './Meter reading/Meterreading';
+import BillList  from './Bill List/BillList'
 
 
 function App() {
@@ -8,6 +10,10 @@ function App() {
     <Router>
         <Routes>
           <Route path="/" element={<DashboardPage />} />
+          <Route path="/meter-reading" element={<MeterReading />} />
+          <Route path="/billList" element={<BillList />} />
+          {/* เพิ่มเส้นทางหน้าอื่นที่นี่ */}
+        
         </Routes>
     </Router>
   );
