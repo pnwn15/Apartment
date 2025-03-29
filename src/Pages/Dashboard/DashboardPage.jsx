2b22bd2b461e7c9087d186fb01eb3662ee28f72f
panawat <panawatsrimuang@gmail.com>
2025-03-29 15:25:12 +0700

    npm run

--- src/Pages/Dashboard/DashboardPage.jsx
+++ src/Pages/Dashboard/DashboardPage.jsx
@@ -1,149 +1,64 @@
 import React, { useState } from 'react';
-import { Grid, FormControl, InputLabel, Select, MenuItem, Card, CardContent, Typography, TextField, Box } from '@mui/material';
+import { Grid, FormControl, InputLabel, Select, MenuItem, TextField, Box, Typography, Card, CardContent } from '@mui/material';
 import CardComponent from './CardComponent';
 import roomData from './roomData.json';
 
 function DashboardPage() {
     const [searchQuery, setSearchQuery] = useState('');
-    const [filter, setFilter] = useState(''); // For floor or other filtering
-    const [floorData, setFloorData] = useState(null);
+    const [filter, setFilter] = useState(''); // เลือกฟิลเตอร์ (เช่น ชั้น, สถานะ)
+    const [selectedFloor, setSelectedFloor] = useState(''); // สำหรับการเลือกชั้น
 
-    const floorContent = {
-        all: (
-            <div className="h-screen">
-                <div className="bg-gray-200">
-                    <h1 className="text-3xl font-bold p-5 text-black">ชั้นที่ 1</h1>
-                </div>
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4 p-5">
-                    {roomData.map((card, index) => (
-                        <div key={index} className="bg-white shadow-lg relative">
-                            {/* Room Image */}
-                            <div
-                                className="w-full h-40 bg-cover bg-center rounded-md"
-                                style={{ backgroundImage: `url(${card.imageUrl})` }}
-                            ></div>
-                            {/* Room Status Badge */}
-                            <div className={`absolute top-0 right-0 m-2 ${getStatusColor(card.statusText)} text-white text-sm px-2 py-1 rounded-md`}>
-                                {card.statusText}
-                            </div>
-                            {/* Card Content */}
-                            <div className="absolute bottom-0 left-0 right-0 p-4 bg-gradient-to-t from-black via-transparent to-transparent">
-                                <h2 className="font-bold text-xl text-white">{card.title}</h2>
-                                <p className="text-white">{card.description}</p>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-            </div>
-        ),
-        available: (
-            <div className="h-screen">
-                <div className="bg-gray-200">
-                    <h1 className="text-3xl font-bold p-5 text-black">ชั้นที่ 2</h1>
-                </div>
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4 p-5">
-                    {roomData.map((card, index) => (
-                        <div key={index} className="bg-white shadow-lg relative">
-                            {/* Room Image */}
-                            <div
-                                className="w-full h-40 bg-cover bg-center rounded-md"
-                                style={{ backgroundImage: `url(${card.imageUrl})` }}
-                            ></div>
-                            {/* Room Status Badge */}
-                            <div className={`absolute top-0 right-0 m-2 ${getStatusColor(card.statusText)} text-white text-sm px-2 py-1 rounded-md`}>
-                                {card.statusText}
-                            </div>
-                            {/* Card Content */}
-                            <div className="absolute bottom-0 left-0 right-0 p-4 bg-gradient-to-t from-black via-transparent to-transparent">
-                                <h2 className="font-bold text-xl text-white">{card.title}</h2>
-                                <p className="text-white">{card.description}</p>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-            </div>
-        ),
-        occupied: (
-            <div className="h-screen">
-                <div className="bg-gray-200">
-                    <h1 className="text-3xl font-bold p-5 text-black">ชั้นที่ 3</h1>
-                </div>
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4 p-5">
-                    {roomData.filter(room => room.statusText === 'ค้างชำระ').map((card, index) => (
-                        <div key={index} className="bg-white shadow-lg relative">
-                            {/* Room Image */}
-                            <div
-                                className="w-full h-40 bg-cover bg-center rounded-md"
-                                style={{ backgroundImage: `url(${card.imageUrl})` }}
-                            ></div>
-                            {/* Room Status Badge */}
-                            <div className={`absolute top-0 right-0 m-2 ${getStatusColor(card.statusText)} text-white text-sm px-2 py-1 rounded-md`}>
-                                {card.statusText}
-                            </div>
-                            {/* Card Content */}
-                            <div className="absolute bottom-0 left-0 right-0 p-4 bg-gradient-to-t from-black via-transparent to-transparent">
-                                <h2 className="font-bold text-xl text-white">{card.title}</h2>
-                                <p className="text-white">{card.description}</p>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-            </div>
-        ),
-        pending: (
-            <div className="h-screen">
-                <div className="bg-gray-200">
-                    <h1 className="text-3xl font-bold p-5 text-black">ชั้นที่ 4</h1>
-                </div>
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4 p-5">
-                    {roomData.filter(room => room.statusText === 'ห้องว่าง').map((card, index) => (
-                        <div key={index} className="bg-white shadow-lg relative">
-                            {/* Room Image */}
-                            <div
-                                className="w-full h-40 bg-cover bg-center rounded-md"
-                                style={{ backgroundImage: `url(${card.imageUrl})` }}
-                            ></div>
-                            {/* Room Status Badge */}
-                            <div className={`absolute top-0 right-0 m-2 ${getStatusColor(card.statusText)} text-white text-sm px-2 py-1 rounded-md`}>
-                                {card.statusText}
-                            </div>
-                            {/* Card Content */}
-                            <div className="absolute bottom-0 left-0 right-0 p-4 bg-gradient-to-t from-black via-transparent to-transparent">
-                                <h2 className="font-bold text-xl text-white">{card.title}</h2>
-                                <p className="text-white">{card.description}</p>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-            </div>
-        ),
-    };
+    const floorData = [
+        { name: 'ชั้นที่ 1', status: 'all' },
+        { name: 'ชั้นที่ 2', status: 'available' },
+        { name: 'ชั้นที่ 3', status: 'occupied' },
+        { name: 'ชั้นที่ 4', status: 'pending' }
+    ];
 
-    // Helper function to determine the status color
+    // ฟังก์ชั่นกรองห้อง
+    const filteredRooms = roomData
+        .filter((room) => {
+            // กรองตามการค้นหาห้อง
+            return room.title.toLowerCase().includes(searchQuery.toLowerCase());
+        })
+        .filter((room) => {
+            // กรองตามชั้นที่เลือก
+            if (selectedFloor === '') return true; // ถ้าไม่ได้เลือกชั้นใดๆ ให้แสดงทุกชั้น
+            return room.floor === selectedFloor; // ตรวจสอบชั้นห้อง
+        })
+        .filter((room) => {
+            // กรองตามสถานะห้องที่เลือก
+            if (filter === '') return true; // ถ้าไม่ได้เลือกสถานะให้แสดงทุกสถานะ
+            return room.statusText.toLowerCase() === filter.toLowerCase(); // ทำให้การเปรียบเทียบไม่สนใจตัวพิมพ์ใหญ่-เล็ก
+        })
+        
+
+    // ฟังก์ชั่นที่ใช้ระบุสีสถานะของห้อง
     function getStatusColor(statusText) {
         if (statusText === 'ค้างชำระ') {
-            return 'bg-red-500'; // Red color for "ค้างชำระ"
+            return 'bg-red-500'; // สีแดงสำหรับ "ค้างชำระ"
         } else if (statusText === 'อัตราการเข้าพัก') {
-            return 'bg-yellow-500'; // Yellow color for "อัตราการเข้าพัก"
+            return 'bg-yellow-500'; // สีเหลืองสำหรับ "อัตราการเข้าพัก"
         } else if (statusText === 'ห้องว่าง') {
-            return 'bg-green-500'; // Green color for "ห้องว่าง"
+            return 'bg-green-500'; // สีเขียวสำหรับ "ห้องว่าง"
         } else {
-            return 'bg-gray-500'; // Default gray for other statuses
+            return 'bg-gray-500'; // สีเทาสำหรับสถานะอื่นๆ
         }
     }
 
-    // Handle filter change (to switch between different floors)
+    // ฟังก์ชั่นที่ใช้สำหรับการเปลี่ยนแปลงฟิลเตอร์
     const handleFilterChange = (event) => {
-        const selectedFloor = event.target.value;
-        setFilter(selectedFloor);
-
-        // Use object to fetch corresponding content for the selected floor
-        setFloorData(floorContent[selectedFloor] || null);
+        setFilter(event.target.value); // เปลี่ยนฟิลเตอร์
     };
 
-    // Handle search input change
+    // ฟังก์ชั่นที่ใช้สำหรับการเปลี่ยนแปลงชั้น
+    const handleFloorChange = (event) => {
+        setSelectedFloor(event.target.value); // เปลี่ยนชั้น
+    };
+
+    // ฟังก์ชั่นที่ใช้สำหรับการค้นหาห้อง
     const handleSearchChange = (event) => {
-        setSearchQuery(event.target.value);
+        setSearchQuery(event.target.value); // เปลี่ยนคำค้นหา
     };
 
     return (
@@ -163,22 +78,19 @@ function DashboardPage() {
                             ผังห้อง
                         </Typography>
                         <Grid container spacing={3} wrap="wrap" justifyContent="start">
-                            {/* Filter Dropdown */}
+                            {/* Filter Dropdown for Status */}
                             <Grid item xs={12} sm={6} md={2}>
                                 <FormControl fullWidth sx={{ maxWidth: 300 }}>
-                                    <InputLabel>ฟิลเตอร์ห้อง</InputLabel>
+                                    <InputLabel>ฟิลเตอร์สถานะ</InputLabel>
                                     <Select
                                         value={filter}
                                         onChange={handleFilterChange}
-                                        label="ฟิลเตอร์ห้อง"
-                                        fullWidth
-                                        sx={{ fontSize: '0.875rem' }}  // Reduce font size
+                                        label="ฟิลเตอร์สถานะ"
                                     >
-                                        <MenuItem value="">เลือกฟิลเตอร์</MenuItem>
-                                        <MenuItem value="all">อัตราการเข้าพัก</MenuItem>
-                                        <MenuItem value="all">ห้องจอง</MenuItem>
-                                        <MenuItem value="all">ค้างชำระ</MenuItem>
-                                        <MenuItem value="pending">ห้องว่าง</MenuItem>
+                                        <MenuItem value="">เลือกสถานะ</MenuItem>
+                                        <MenuItem value="ค้างชำระ">ค้างชำระ</MenuItem>
+                                        <MenuItem value="อัตราการเข้าพัก">อัตราการเข้าพัก</MenuItem>
+                                        <MenuItem value="ห้องว่าง">ห้องว่าง</MenuItem>
                                     </Select>
                                 </FormControl>
                             </Grid>
@@ -194,21 +106,21 @@ function DashboardPage() {
                                     sx={{ maxWidth: 300 }}
                                 />
                             </Grid>
+
+                            {/* Floor Dropdown */}
                             <Grid item xs={12} sm={6} md={2}>
                                 <FormControl fullWidth sx={{ maxWidth: 300 }}>
                                     <InputLabel>เลือกชั้น</InputLabel>
                                     <Select
-                                        value={filter}
-                                        onChange={handleFilterChange}
+                                        value={selectedFloor}
+                                        onChange={handleFloorChange}
                                         label="ชั้น"
-                                        fullWidth
-                                        sx={{ fontSize: '0.875rem' }} // ลดขนาดฟอนต์
                                     >
                                         <MenuItem value="">เลือกชั้น</MenuItem>
-                                        <MenuItem value="all">1</MenuItem>
-                                        <MenuItem value="available">2</MenuItem>
-                                        <MenuItem value="occupied">3</MenuItem>
-                                        <MenuItem value="pending">4</MenuItem>
+                                        <MenuItem value="1">ชั้นที่ 1</MenuItem>
+                                        <MenuItem value="2">ชั้นที่ 2</MenuItem>
+                                        <MenuItem value="3">ชั้นที่ 3</MenuItem>
+                                        <MenuItem value="4">ชั้นที่ 4</MenuItem>
                                     </Select>
                                 </FormControl>
                             </Grid>
@@ -217,18 +129,41 @@ function DashboardPage() {
                 </Box>
             </Grid>
 
-            {/* Show Floor Content */}
+            {/* Show Filtered Rooms */}
             <Grid item xs={12}>
                 <Box>
-                    {floorData && (
-                        <Grid item xs={12}>
-                            <Card>
-                                <CardContent sx={{ padding: 0, margin: 0 }}> {/* Remove padding and margin */}
-                                    <Typography variant="body1">{floorData}</Typography>
-                                </CardContent>
-                            </Card>
-                        </Grid>
-                    )}
+                    <Grid container spacing={4}>
+                        {filteredRooms.map((room, index) => (
+                            <Grid item xs={12} sm={6} lg={4} key={index}>
+                                <Card>
+                                    <CardContent>
+                                        <div className="relative">
+                                            {/* Room Image */}
+                                            <div
+                                                className="w-full h-40 bg-cover bg-center rounded-md"
+                                                style={{ backgroundImage: `url(${room.imageUrl})` }}
+                                            ></div>
+                                            {/* Room Status Badge */}
+                                            <div
+                                                className={`absolute top-0 right-0 m-2 ${getStatusColor(room.statusText)} text-white text-sm px-2 py-1 rounded-md`}
+                                            >
+                                                {room.statusText}
+                                            </div>
+                                            {/* Card Content */}
+                                            <div className="absolute bottom-0 left-0 right-0 p-4 bg-gradient-to-t from-black via-transparent to-transparent">
+                                                <Typography variant="h6" className="font-bold text-white">
+                                                    {room.title}
+                                                </Typography>
+                                                <Typography variant="body2" className="text-white">
+                                                    {room.description}
+                                                </Typography>
+                                            </div>
+                                        </div>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                        ))}
+                    </Grid>
                 </Box>
             </Grid>
         </Grid>
